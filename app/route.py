e2a240d84bee3f101bd8aea53b78973b38ded7b1
kartik khandelwal <khandelwal.kartik4@gmail.com>
2017-04-09 21:47:04 +0000

    Update route.py
    Bluetooth connectivity with arduino

--- app/route.py
+++ app/route.py
@@ -1,57 +1,51 @@
-import RPi.GPIO as GPIO
+from flask import Flask,render_template,request
 import serial
-from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-# Adding arduino communications with bluetooth 
-bluetoothSerial = serial.Serial( "/dev/rfcomm1", baudrate=9600 )
+bluetoothSerial = serial.Serial("/dev/rfcomm1", baudrate=9600)
 
-
-GPIO.setmode(GPIO.BOARD)
-
-# Create a dictionary called pins to store the pin number, name, and pin state:
 pins = {
-   24 : {'name' : 'GPIO 24', 'state' : GPIO.LOW},
-   12 : {'name' : 'GPIO 12', 'state' : GPIO.LOW}
-   }
+        1 : {'name' : 'Light' , 'state' : False },
+        2 : {'name' : 'Fan'   , 'state' : False }
+       }
 
-# Set each pin as an output and make it low:
-for pin in pins:
-   GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
+@app.route('/')
+def index():
+    templatedata = {
+                   'pins' : pins
+                  }
+   
+    return render_template('main.html', **templateData)
 
-@app.route("/")
-def main():
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
-   # Put the pin dictionary into the template data dictionary:
-   templateData = {
-      'pins' : pins
-      }
-   # Pass the template data into the template main.html and return it to the user
-   return render_template('main.html', **templateData)
 
-# The function below is executed when someone requests a URL with the pin number and action in it:
-@app.route("/<changePin>/<action>")
-def action(changePin, action):
-   # Convert the pin from the URL into an integer:
-   changePin = int(changePin)
+@app.route("/<value>/<action>")
+def action(value,action):
+    
+   changePin = int(value)
    # Get the device name for the pin being changed:
    deviceName = pins[changePin]['name']
    # If the action part of the URL is "on," execute the code indented below:
    if action == "on":
-      # Set the pin high:
-      GPIO.output(changePin, GPIO.HIGH)
-      # Save the status message to be passed into the template:
-      message = "Turned " + deviceName + " on."
-   if action == "off":
-      GPIO.output(changePin, GPIO.LOW)
-      message = "Turned " + deviceName + " off."
+       if changePin == 1:
+           bluetoothSerial.write(1)
+        
+       if changePin == 2:
+           bluetoothSerial.write(2)
 
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
+       message = 'Turned' + deviceName + 'ON'
+           
+       pins[changePin]['state'] = True
+       
+      # Save the status message to be passed into the template:
+      
+   if action == "off":
+       if changePin == 1:
+           bluetoothSerial.write(0)
+       if changePin == 2:
+           bluetoothSerial.write(3)
+       message = 'Turned' + deviceName + 'OFF'
+       pins[changePin]['state'] = False   
 
    # Along with the pin dictionary, put the message into the template data dictionary:
    templateData = {
@@ -60,6 +54,5 @@ def action(changePin, action):
 
    return render_template('main.html', **templateData)
 
-
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
